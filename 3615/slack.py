0ec5e2f9cd244538f5a72dd35ba208435aa6b543
Micha Mazaheri <micha@mazaheri.me>
2015-05-21 16:48:34 +0200

    Fixed missing Slack username.

--- 3615/slack.py
+++ 3615/slack.py
@@ -3,6 +3,8 @@ import requests
 SLACK_TOKEN = "xoxp-3673911929-3918993035-5010858377-0403b1"
 SLACK_CHANNEL = "C050AQ1AB" # `minitel` channel
 
+username_cache = {}
+
 def get_slack_messages():
 
   # GET /channels.history
@@ -30,6 +32,12 @@ def get_slack_messages():
         username=message['username'].replace('\n', ''),
         message=message['text'].replace('\n', ''),
       )
+    # if has a user
+    elif message.get('user'):
+      slack_messages_text += '{username} : {message}\n'.format(
+        username=get_slack_username(message.get('user')).replace('\n', ''),
+        message=message['text'].replace('\n', ''),
+      )
     # no username, display only text
     else:
       slack_messages_text += '{message}\n'.format(
@@ -38,6 +46,28 @@ def get_slack_messages():
 
   return slack_messages_text
 
+def _fetch_slack_username(user_id):
+  r = requests.get(
+      url="https://slack.com/api/users.info",
+      params = {
+          "token":SLACK_TOKEN,
+          "user":user_id,
+      },
+  )
+  return r.json()['user']['name']
+
+def get_slack_username(user_id):
+  # check if in cache
+  if username_cache.get(user_id):
+    return username_cache.get(user_id)
+
+  # fetch username
+  username = _fetch_slack_username(user_id)
+
+  # cache
+  username_cache[user_id] = username
+
+  return username
 
 def post_slack_message(text, username='Minitel'):
   requests.post(

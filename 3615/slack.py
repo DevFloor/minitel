b1774ac57c01017cc8d428027fb47e3b6f220bbf
Micha Mazaheri <micha@mazaheri.me>
2015-05-22 09:56:23 +0200

    Decoded HTML entities from Slack.

--- 3615/slack.py
+++ 3615/slack.py
@@ -1,9 +1,12 @@
 import requests
+import HTMLParser
 
 SLACK_TOKEN = "xoxp-3673911929-3918993035-5010858377-0403b1"
 SLACK_CHANNEL = "C050AQ1AB" # `minitel` channel
 
 username_cache = {}
+
+html_parser = HTMLParser.HTMLParser()
 
 def get_slack_messages():
 
@@ -26,22 +29,26 @@ def get_slack_messages():
     # ignore messages without text
     if not message.get('text'):
       continue
+
+    message_text = message['text'].replace('\n', '')
+    message_text = html_parser.unescape(message_text)
+
     # if has a username
     if message.get('username'):
       slack_messages_text += '{username} : {message}\n'.format(
         username=message['username'].replace('\n', ''),
-        message=message['text'].replace('\n', ''),
+        message=message_text,
       )
     # if has a user
     elif message.get('user'):
       slack_messages_text += '{username} : {message}\n'.format(
         username=get_slack_username(message.get('user')).replace('\n', ''),
-        message=message['text'].replace('\n', ''),
+        message=message_text,
       )
     # no username, display only text
     else:
       slack_messages_text += '{message}\n'.format(
-        message=message['text'].replace('\n', ''),
+        message=message_text,
       )
 
   return slack_messages_text
